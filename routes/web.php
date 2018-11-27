d6854e549834c156072ecff433b9c969e43928aa
无尘阁 <ligui0506@126.com>
2018-11-27 16:16:05 +0800

    php xdebug

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 Route::get('/', function () {
     // phpinfo();
-    // for ($i=0; $i < 10; $i++) {
-    //     logger('logger log' . $i);
-    // }
+     for ($i=0; $i < 10; $i++) {
+         logger('logger log' . $i);
+     }
+     $a=1;
+     $b=2;
+     $c=&$b;
+     $c=3;
+     $d=$a*$c;
     clock('clock log');
     \DB::enableQueryLog();
     $user = App\Models\User::all();

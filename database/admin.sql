68b2faa24411edefe65e50e7010e9352e3c7c0f1
无尘阁 <ligui0506@126.com>
2018-12-07 14:12:59 +0800

    sql change

--- database/admin.sql
+++ database/admin.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.18, for osx10.12 (x86_64)
 --
--- Host: localhost    Database: fund
+-- Host: localhost    Database: api
 -- ------------------------------------------------------
 -- Server version	5.7.18
 
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2018-10-30 02:19:17'),(2,0,9,'超级管理','fa-tasks',NULL,NULL,NULL,'2018-11-09 02:33:27'),(3,2,10,'管理员','fa-users','auth/users',NULL,NULL,'2018-11-09 02:33:27'),(4,2,11,'角色','fa-user','auth/roles',NULL,NULL,'2018-11-09 02:33:27'),(5,2,12,'权限','fa-ban','auth/permissions',NULL,NULL,'2018-11-09 02:33:27'),(6,2,13,'菜单','fa-bars','auth/menu',NULL,NULL,'2018-11-09 02:33:27'),(7,2,14,'操作日志','fa-history','auth/logs',NULL,NULL,'2018-11-09 02:33:27'),(8,0,2,'返回消息','fa-anchor','/errors',NULL,'2018-10-30 18:28:40','2018-11-06 02:15:22'),(9,0,3,'用户管理','fa-user','/users',NULL,'2018-10-31 18:49:32','2018-11-06 02:15:22'),(10,0,4,'基金列表','fa-money','/products',NULL,'2018-11-01 10:47:05','2018-11-06 02:15:22'),(11,0,6,'版本管理','fa-copyright','/versions',NULL,'2018-11-05 06:52:14','2018-11-06 02:15:22'),(12,0,5,'订单管理','fa-yen','/orders',NULL,'2018-11-06 02:12:39','2018-11-06 02:15:22'),(13,0,7,'提现管理','fa-battery-2','/moneys',NULL,'2018-11-07 05:46:33','2018-11-07 05:46:44'),(14,0,8,'新闻列表','fa-beer','/news',NULL,'2018-11-09 02:33:18','2018-11-09 02:33:27'),(15,0,0,'反馈管理','fa-feed','/feedbacks',NULL,'2018-11-15 03:24:04','2018-11-15 03:24:04');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2018-11-26 05:56:33'),(2,0,2,'超级管理','fa-tasks',NULL,NULL,NULL,'2018-11-26 05:57:07'),(3,2,3,'管理员','fa-users','auth/users',NULL,NULL,'2018-11-26 05:55:37'),(4,2,4,'角色','fa-user','auth/roles',NULL,NULL,'2018-11-26 05:55:31'),(5,2,5,'权限','fa-ban','auth/permissions',NULL,NULL,'2018-11-26 05:55:52'),(6,2,6,'菜单','fa-bars','auth/menu',NULL,NULL,'2018-11-26 05:55:59'),(7,2,7,'后台日志','fa-history','auth/logs',NULL,NULL,'2018-11-26 05:56:11'),(8,0,8,'Redis','fa-database','redis',NULL,'2018-11-26 05:51:23','2018-11-26 05:56:22'),(9,0,0,'错误消息','fa-anchor','/errors',NULL,'2018-11-26 06:04:02','2018-11-26 06:04:02'),(10,0,0,'文章','fa-align-left','/articles',NULL,'2018-11-26 06:42:04','2018-11-26 06:42:04'),(11,0,0,'版本管理','fa-flag','/versions',NULL,'2018-11-26 07:36:22','2018-11-26 07:36:22');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -31,7 +31,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_permissions` WRITE;
 /*!40000 ALTER TABLE `admin_permissions` DISABLE KEYS */;
-INSERT INTO `admin_permissions` VALUES (1,'All permission','*','','*',NULL,NULL),(2,'Dashboard','dashboard','GET','/',NULL,NULL),(3,'Login','auth.login','','/auth/login\r\n/auth/logout',NULL,NULL),(4,'User setting','auth.setting','GET,PUT','/auth/setting',NULL,NULL),(5,'Auth management','auth.management','','/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs',NULL,NULL);
+INSERT INTO `admin_permissions` VALUES (1,'All permission','*','','*',NULL,NULL),(2,'Dashboard','dashboard','GET','/',NULL,NULL),(3,'Login','auth.login','','/auth/login\r\n/auth/logout',NULL,NULL),(4,'User setting','auth.setting','GET,PUT','/auth/setting',NULL,NULL),(5,'Auth management','auth.management','','/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs',NULL,NULL),(6,'Redis Manager','ext.redis-manager',NULL,'/redis*','2018-11-26 05:51:23','2018-11-26 05:51:23');
 /*!40000 ALTER TABLE `admin_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,7 +71,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_roles` WRITE;
 /*!40000 ALTER TABLE `admin_roles` DISABLE KEYS */;
-INSERT INTO `admin_roles` VALUES (1,'Administrator','administrator','2018-10-30 02:18:06','2018-10-30 02:18:06');
+INSERT INTO `admin_roles` VALUES (1,'Administrator','administrator','2018-11-26 05:48:33','2018-11-26 05:48:33');
 /*!40000 ALTER TABLE `admin_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,8 +90,18 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin_users` WRITE;
 /*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
-INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$a15X9U9VDMqDjPNZ4VSPCetDf5omOr6/JxBnlxScAd8i3yyQiimoK','Administrator',NULL,NULL,'2018-10-30 02:18:06','2018-10-30 02:18:06');
+INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$3srZLFWY1CmKn7y9lq0osO5XBGvWbEd1nn/GnvpM.nvMGcvCM7CXm','Administrator',NULL,NULL,'2018-11-26 05:48:33','2018-11-26 05:48:33');
 /*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `errors`
+--
+
+LOCK TABLES `errors` WRITE;
+/*!40000 ALTER TABLE `errors` DISABLE KEYS */;
+INSERT INTO `errors` VALUES (1,0,'zh_CN','成功!!','2018-11-22 11:37:43','2018-11-26 06:23:59'),(2,1,'zh_CN','失败，请重试!','2018-11-22 11:37:43','2018-11-22 11:37:43'),(3,100401,'zh_CN','未登录!','2018-11-22 11:37:43','2018-11-22 11:37:43'),(4,100402,'zh_CN','缺少参数!','2018-11-22 11:37:43','2018-11-22 11:37:43'),(5,100403,'zh_CN','参数格式错误!','2018-11-22 11:37:43','2018-11-22 11:37:43'),(6,100404,'zh_CN','请求方法错误!','2018-11-22 11:37:43','2018-11-22 11:37:43'),(7,100429,'zh_CN','连接次数过多，请稍后再试!','2018-11-22 11:37:43','2018-11-22 11:37:43'),(8,100500,'zh_CN','服务器错误，请联系管理员!','2018-11-22 11:37:43','2018-11-22 11:37:43'),(9,100502,'zh_CN','服务器压力太大，请联系管理员!','2018-11-22 11:37:43','2018-11-22 11:37:43');
+/*!40000 ALTER TABLE `errors` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -103,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-26 15:59:15
+-- Dump completed on 2018-12-07 14:12:47
